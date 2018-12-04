c12e2e1b88452b58b25d7b83d2461681c6c53649
sasharadar <admin@microsoft.com>
2018-12-04 08:47:20 +0300

    two changes: 1. Added Y-coordinate borders to player movement 2. change sphere Y-position, made it closer to the ground

--- SdelalZa1Day/Assets/GameplayConstants.cs
+++ SdelalZa1Day/Assets/GameplayConstants.cs
@@ -3,5 +3,12 @@ using UnityEditor;
 
 public class GameplayConstants : MonoBehaviour
 {
-    public const int rowWidth = 5; //ширина ряда кубов задаётся тут!
+    //ширина ряда кубов задаётся тут!
+    public const int rowWidth = 5;
+    
+    //ограничение по высоте, от
+    public const int groundFrom = 0;
+    
+    //ограничение по высоте, до
+    public const int groundTo = 3;
 }
