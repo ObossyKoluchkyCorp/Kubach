f7b9d878635aaeb4a50d581e5eeeb7c1c3704e04
Papupoca <45541945+Papupoca@users.noreply.github.com>
2018-12-05 18:34:19 +0300

    Обновлённое меню
    фул

---
+++ SdelalZa1Day/Assets/MainMenuScripts/CursorsS.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorsS : MonoBehaviour // Скрипт для изменения цвета курсора в таргет зоне 
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    public AudioClip ButtonAud;
+
+    public AudioSource Audios;
+
+
+    void Start()
+    {
+
+        Audios.clip = ButtonAud;
+    }
+    void OnMouseEnter()
+    {
+        Audios.Play();
+    }
+
+    void OnMouseOver()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+
+    }
+
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+
+    }
+}
